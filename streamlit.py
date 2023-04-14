7607300a017dd6082781640d82956c36df7811bf
nptlong93 <118968765+nptlong93@users.noreply.github.com>
2023-04-14 23:15:52 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -25,9 +25,10 @@ def load_data():
 def run_model(data):
     # 2.2. Remove missing values
     #data = data.reset_index(drop=True)
+    source = np.array(data["name_description_wt"])
     tf = TfidfVectorizer(analyzer='word')
     #data = data.dropna(subset=['name_description_wt']).reset_index(drop=True)
-    tfidf_matrix = tf.fit_transform(data["name_description_wt"])
+    tfidf_matrix = tf.fit_transform(source)
     model = cosine_similarity(tfidf_matrix, tfidf_matrix)
     return model
 
