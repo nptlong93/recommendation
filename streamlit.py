cc1e564969a1a0c3516047cc68cd8ced36b22fbc
nptlong93 <118968765+nptlong93@users.noreply.github.com>
2023-04-14 18:51:15 +0700

    update large file

--- streamlit.py
+++ streamlit.py
@@ -31,7 +31,7 @@ from streamlit_option_menu import option_menu
 data = pd.read_csv("Products_ThoiTrangNam_clean.csv", encoding='utf-8')
 #df = spark.read.csv('Products_ThoiTrangNam_rating_raw.csv', header=True, inferSchema=True, sep=r'\t')
 #Load models 
-with open("Model/cosine_similarities.pkl", 'rb') as file:  
+with open("cosine_similarities.pkl", 'rb') as file:  
     model = pickle.load(file)
 # Load als_model folder
 #als_model = ALSModel.load("Model/als_model")
