7ba1b6d00f46935aafae65eec133c74e13d871ec
nptlong93 <118968765+nptlong93@users.noreply.github.com>
2023-04-16 16:59:39 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -94,6 +94,7 @@ elif choice == 'Content-based Filtering':
         # Get product rating
         product_rating = data.iloc[idx]['rating']
         # Get product image
+        data[['image']] = data[['image']].astype(str)
         product_image = data.iloc[idx]['image']
         # Create a seleted image
         selected_image = image_select(label= "Select image",images=[product_image], captions=[product_name], use_container_width = False)
