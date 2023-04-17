6752e5f82046eebb89fa28a916fd16c7f26f1c0f
nptlong93 <118968765+nptlong93@users.noreply.github.com>
2023-04-17 19:34:19 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -153,7 +153,7 @@ elif choice == 'Collaborative Filtering':
         results = [user for user in usern if query.lower() in user.lower()]
         return results
     search = st.text_input('Username: ', max_chars=100)
-    #textb = st.text_input('Password: ')
+    textb = st.text_input('Password: ', max_chars=100)
     # if no user is found, show error message
     if not search:
         st.error("Your username is not correct. Please try again")
