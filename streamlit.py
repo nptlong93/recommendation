b3d9d6e88d513dae3ef712befda4dcf1d2d51f1e
nptlong93 <118968765+nptlong93@users.noreply.github.com>
2023-04-17 19:50:58 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -170,7 +170,7 @@ elif choice == 'Collaborative Filtering':
             # Show product_name based on selected user_id
             st.write("### Product_name: ")
             # Get list of product_name based on user in search
-            product_name = data2[data2['user'] == ]['product_name'].unique()                     
+            product_name = data2[data2['user'] == search]['product_name'].unique()                     
             # Align the product name and product price under the product image
             for i in product_name:
                 # Get index of product
